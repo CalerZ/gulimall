6f82721803c7850695ce6c8088e60f7e7d92fa2f
caler <18329776952@163.com>
2020-11-18 17:32:56 +0800

    jenkins报错，去掉一些无用的代码包

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/entity/CategoryEntity.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/entity/CategoryEntity.java
@@ -7,12 +7,12 @@ import com.baomidou.mybatisplus.annotation.TableName;
 
 import java.io.Serializable;
 import java.util.List;
-import com.sun.istack.internal.NotNull;
 import lombok.Data;
 
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 /**
